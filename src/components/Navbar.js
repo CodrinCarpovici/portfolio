513b1ef47f92fcf41856a2efeb76c1bd2eccab46
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-04-11 18:12:33 +0100

    first commit, initial set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import defaultBG from "../assets/cgc-logo/defaultBG.png"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/" alt="logo">
+          <img className="img-fluid" src={defaultBG} />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
