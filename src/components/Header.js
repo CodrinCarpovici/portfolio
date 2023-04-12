9a7b34656e05ceecb65fb27fae8798954c971c66
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-04-12 13:52:26 +0100

    header set up done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header d-flex align-items-center">
+      <div className="container main-offer">
+        <h1 className="display-title">CODRIN G. CARPOVICI</h1>
+        <h2 className="sub-title">WEB DEVELOPER</h2>
+        <button className="about-btn">ABOUT ME</button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
