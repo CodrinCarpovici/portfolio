942dbac10ae691fe4d6fe6e5eebb8b05a7ad88e9
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-04-12 20:23:23 +0100

    footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,36 @@
 import React from "react";
 import logo from "../assets/cgc-logo/defaultBG.png";
+import github from "../assets/icons/github.png";
+import linkedin from "../assets/icons/linkedin.png";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer pt-3">
       <div className="container">
         <div className="row">
           <div className="col">
             <a href="/">
-              <img src={logo} alt="logo" />
+              <img src={logo} alt="logo" className="logo" />
             </a>
           </div>
-          <div className="col"></div>
-          <div className="col"></div>
+          <div className="col text-center">
+            <a href="https://github.com/CodrinCarpovici" className="">
+              <img src={github} alt="github logo" className="social-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/codrin-gabriel-c-31b4b899/" className="">
+              <img src={linkedin} alt="linkedin logo" className="social-icon" />
+            </a>
+            <div className="">
+              <p className="copyright">
+                Copyright© 2023 CGC, Inc. All rights reserved.
+              </p>
+            </div>
+          </div>
+          <div className="col d-flex flex-column align-items-end">
+            <a href="/" className="footer-link">Home</a>
+            <a href="/projects" className="footer-link">Projects</a>
+            <a href="/contact" className="footer-link">Contact</a>
+          </div>
         </div>
       </div>
     </footer>
