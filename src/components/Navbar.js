22ed4a3c9823f6842d8517430d42a51bf089e1b3
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-04-12 13:07:04 +0100

    styled custom toggler icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
           <img className="img-fluid logo" src={defaultBG} alt="logo"/>
         </a>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavDropdown"
@@ -17,7 +17,9 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="toggler-icon top-bar"></span>
+          <span className="toggler-icon middle-bar"></span>
+          <span className="toggler-icon bottom-bar"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav ms-auto mb-lg-0">
