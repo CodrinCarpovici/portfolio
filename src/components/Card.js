e6edcd6a7942e0d2d9dea474f1ac61f7a6605055
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-04-18 20:05:37 +0100

    fixed projects css

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,11 @@ const Card = (props) => {
         <div className="img-container">
           <img src={props.img} alt="project" className="img-fluid" />
         </div>
-        <h2 className="top-left card-title">{props.title}</h2>
-        <h3 className="center-left card-sub-title">{props.subTitle}</h3>
-        <button className="button bottom-left case-button">Case Study</button>
+        <div className="centered">
+          <h2 className="card-title">{props.title}</h2>
+          <h3 className="card-sub-title">{props.subTitle}</h3>
+          <button className="button case-button">Case Study</button>
+        </div>
       </div>
     </article>
   );
