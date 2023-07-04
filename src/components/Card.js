85262b31897644ce359707fc728e714dfd4524ed
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-07-04 21:59:51 +0100

    added transitions for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Card = (props) => {
   return (
-    <div className="card-container">
+    <motion.div
+      className="card-container"
+      whileHover={{ scale: 1.05 }}
+      transition={{ type: "spring", stiffness: 100, damping: 20 }}
+    >
       <div className="mask"> </div>
       <div className="img-container">
         <img src={props.img} alt="project" className="img-fluid" />
@@ -10,9 +15,17 @@ const Card = (props) => {
       <div className="centered">
         <h2 className="card-title">{props.title}</h2>
         <h3 className="card-sub-title">{props.subTitle}</h3>
-        <button className="button case-button">Case Study</button>
+        <motion.button
+          className="button case-button"
+          whileHover={{
+            scale: 1.05,
+            textShadow: "0px 0px 4px gray",
+          }}
+        >
+          Case Study
+        </motion.button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
