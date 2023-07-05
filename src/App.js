9884774b6656d609021dce3236f55a104e6d2ebd
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-07-05 23:45:12 +0100

    set up for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import './App.scss';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
-import CasePage from './components/CasePage';
-import { useState, useEffect } from 'react';
+import "./App.scss";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import CasePage from "./components/CasePage";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     if (openModal) {
-      document.body.classList.add('modal-open');
+      document.body.classList.add("modal-open");
     } else {
-      document.body.classList.remove('modal-open');
+      document.body.classList.remove("modal-open");
     }
   }, [openModal]);
 
   return (
     <div className="App">
-      <Navbar setOpenModal={setOpenModal}/>
-      <Header setOpenModal={setOpenModal}/>
-      <Projects />
+      <Navbar setOpenModal={setOpenModal} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header id="home" setOpenModal={setOpenModal} />
+              <Projects />
+            </>
+          }
+        ></Route>
+      </Routes>
+
       <CasePage />
-      <Contact open={openModal} onClose={() => setOpenModal(false)}/>
+      <Contact open={openModal} onClose={() => setOpenModal(false)} />
       <Footer />
     </div>
   );
--- src/components/caseData.js
+++ src/components/caseData.js
@@ -1,6 +1,20 @@
+import restaurant from "../assets/images/restaurant.jpg";
 import mechanic from "../assets/images/mechanic.jpg";
+import developer from "../assets/images/developer.jpg";
+import luckyshrub from "../assets/images/design garden.jpg";
 
 const data = [
+  {
+    title: "Little Lemon",
+    titleImg: restaurant,
+    description:
+      `Eurokit is a Hydraulics business. The company needed to expand and part of the expansion involved getting a more permanent online presence. They needed a modern website to present their products and have customers be able to contact them through it. Through the use of a modern Tech Stack I’ve achieved just that and went through the whole Software Development Lifecycle for this project. I picked colors that matched the company’s logo and I’ve ensured responsiveness and a smooth process for the company to receive emails from the customers using the contact form on the website. Each product page is dynamically generated based on the data that is being passed.`,
+    goals: ["RESPONSIVE", "MODERN", "SEO OPTIMIZED"],
+    tech: ["ReactJS", "Bootstrap", "JavaScript", "Semantic HTML5", "CSS/SCSS"],
+    achievements: ["Modern Web Application", "Clean and Simple Design", "Sales increased by 20%"],
+    live: "https://eurokit.org/",
+    github: "https://github.com/CodrinCarpovici/eurokit-hidraulica"
+  },
   {
     title: "Eurokit",
     titleImg: mechanic,
@@ -12,7 +26,28 @@ const data = [
     live: "https://eurokit.org/",
     github: "https://github.com/CodrinCarpovici/eurokit-hidraulica"
   },
-
+  {
+    title: "Dev Portfolio",
+    titleImg: developer,
+    description:
+      `Eurokit is a Hydraulics business. The company needed to expand and part of the expansion involved getting a more permanent online presence. They needed a modern website to present their products and have customers be able to contact them through it. Through the use of a modern Tech Stack I’ve achieved just that and went through the whole Software Development Lifecycle for this project. I picked colors that matched the company’s logo and I’ve ensured responsiveness and a smooth process for the company to receive emails from the customers using the contact form on the website. Each product page is dynamically generated based on the data that is being passed.`,
+    goals: ["RESPONSIVE", "MODERN", "SEO OPTIMIZED"],
+    tech: ["ReactJS", "Bootstrap", "JavaScript", "Semantic HTML5", "CSS/SCSS"],
+    achievements: ["Modern Web Application", "Clean and Simple Design", "Sales increased by 20%"],
+    live: "https://eurokit.org/",
+    github: "https://github.com/CodrinCarpovici/eurokit-hidraulica"
+  },
+  {
+    title: "Lucky Shrub",
+    titleImg: luckyshrub,
+    description:
+      `Eurokit is a Hydraulics business. The company needed to expand and part of the expansion involved getting a more permanent online presence. They needed a modern website to present their products and have customers be able to contact them through it. Through the use of a modern Tech Stack I’ve achieved just that and went through the whole Software Development Lifecycle for this project. I picked colors that matched the company’s logo and I’ve ensured responsiveness and a smooth process for the company to receive emails from the customers using the contact form on the website. Each product page is dynamically generated based on the data that is being passed.`,
+    goals: ["RESPONSIVE", "MODERN", "SEO OPTIMIZED"],
+    tech: ["ReactJS", "Bootstrap", "JavaScript", "Semantic HTML5", "CSS/SCSS"],
+    achievements: ["Modern Web Application", "Clean and Simple Design", "Sales increased by 20%"],
+    live: "https://eurokit.org/",
+    github: "https://github.com/CodrinCarpovici/eurokit-hidraulica"
+  },
 ];
 
 export default data;
