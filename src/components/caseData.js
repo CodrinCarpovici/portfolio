b18a5b53ce2fc070937c741106ccb5eeb4fdadd8
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-07-05 23:20:29 +0100

    fixed jank when modal opens

--- src/components/caseData.js
+++ src/components/caseData.js
@@ -8,7 +8,7 @@ const data = [
       `Eurokit is a Hydraulics business. The company needed to expand and part of the expansion involved getting a more permanent online presence. They needed a modern website to present their products and have customers be able to contact them through it. Through the use of a modern Tech Stack I’ve achieved just that and went through the whole Software Development Lifecycle for this project. I picked colors that matched the company’s logo and I’ve ensured responsiveness and a smooth process for the company to receive emails from the customers using the contact form on the website. Each product page is dynamically generated based on the data that is being passed.`,
     goals: ["RESPONSIVE", "MODERN", "SEO OPTIMIZED"],
     tech: ["ReactJS", "Bootstrap", "JavaScript", "Semantic HTML5", "CSS/SCSS"],
-    achievements: ["Modern Web Application", "Clean and Simple Design", "Sales increased by 50%"],
+    achievements: ["Modern Web Application", "Clean and Simple Design", "Sales increased by 20%"],
     live: "https://eurokit.org/",
     github: "https://github.com/CodrinCarpovici/eurokit-hidraulica"
   },
